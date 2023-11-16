c56edfd20e14b5a5545c8d276cc20ab9c20adbae
Darren Brain <darren.brain@gmail.com>
2023-11-16 13:45:12 -0500

    Add svg file output

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-const shapes = require('./lib/shapes');
+const inquirer = require("inquirer")
+const {Circle, Square, Triangle} = require('./lib/shapes');
+const {writeFile} = require("fs").promises
+var shape;
 
-var shape = new shapes.Triangle("blue");
-console.log(shape.render());
+const questions = [
+    {
+    type: "list",
+    name: "shapeType",
+    prompt: "Select your background shape : ",
+    choices : ["Circle", "Triangle", "Square"]
+    },
+    {
+    type: "input",
+    name: "shapeColor",
+    prompt: "Color of shape : "},
+    {
+    type: "input",
+    name: "initials",
+    prompt: "Enter up to 3 characters : "},
+    {
+    type: "input",
+    name: "textColor",
+    prompt: "Color of the text : "}
+]
+
+inquirer.prompt(questions)
+    .then( answers => {
+        switch(answers.shapeType) {
+            case "Circle" : 
+                shape = new Circle(answers.shapeColor)
+                break
+            case "Square" :
+                shape = new Square(answers.shapeColor)
+                break
+            case "Triangle" : 
+                shape = new Triangle(answers.shapeColor)
+                break
+        } 
+        outputStr = `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+                ${shape.render()}
+
+                <text x="150" y="${shape.yVal}" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.initials}</text>
+
+            </svg>`
+        writeFile(`./output/${answers.initials}-${answers.shapeType}-${answers.shapeColor}-${answers.textColor}.svg`, outputStr)
+    })
+
